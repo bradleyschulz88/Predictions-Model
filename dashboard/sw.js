5fc3e5021bfd0faf02004cb041f0400dd4292777
Bradley Schulz <bradleyschulz88@hotmail.com>
2026-06-16 14:58:45 +1000

    Mobile Design

--- dashboard/sw.js
+++ dashboard/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "predictions-dashboard-v31";
+const CACHE = "predictions-dashboard-v32";
 const ASSETS = ["./", "./index.html", "./app.js", "./styles.css", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
